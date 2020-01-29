f7d07475447dde0fa832315dd66a5805a097e36a
Muhammad Siraj <60263762+SirajKarim@users.noreply.github.com>
2020-01-28 18:51:54 -0800

    Add files via upload
    Network Programming Client First Run Server and then Client

---
+++ NP_CLIENT1/NP_CLIENT1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NP_CLIENT1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Loopback;
+            IPEndPoint ep = new IPEndPoint(ip,2000);
+            Socket sk = new Socket(ip.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+            sk.Connect(ep);
+            Console.WriteLine("New Client Connected");
+        }
+    }
+}
