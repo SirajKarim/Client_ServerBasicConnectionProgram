8df654754e396029fbbaa538081446513c0470af
Muhammad Siraj <60263762+SirajKarim@users.noreply.github.com>
2020-01-28 18:58:11 -0800

    Add files via upload

---
+++ NP_SERVER1/NP_SERVER1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP_SERVER1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Loopback;
+            IPEndPoint ep = new IPEndPoint(ip, 2000);
+            Socket sk = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            sk.Bind(ep);
+            Console.WriteLine("Server Waiting");
+            sk.Listen(10);
+            Socket clsk = sk.Accept();
+            Console.WriteLine("Connected");
+        }
+    }
+}
